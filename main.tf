63af8aeaa970edbf6713383dfc9fbaf4744b8408
slyandc <18745271+slyandc@users.noreply.github.com>
2023-12-01 16:09:24 +0000

    load balancer

--- main.tf
+++ main.tf
@@ -47,6 +47,44 @@ resource "aws_instance" "blog" {
     Name = "Learning Terraform"
   }
 }
+
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
+
+  load_balance_type = "application"
+
+  vpc_id  = module.blog_vpc.vpc.id
+  subnets = module.blog_vpc.public_subnets
+  security_groups = module.blog_sg.security_group_id
+ 
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol         = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = {  
+       my_target = {
+        target_id = aws_instance.blog.id
+        port = 80
+       }
+     }
+    }
+  ]
+
+  http_tcp_listeners = [
+    {
+     port = 80
+     protocol = "http"
+     target_group_index = 0
+     }
+  ]
+ tags = {
+    Environment = "Dev"
+    }
+
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
