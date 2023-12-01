0337195b8ac019279a95dc2c7d4fc9086f0ce628
slyandc <18745271+slyandc@users.noreply.github.com>
2023-12-01 18:17:42 +0000

    removealb target

--- main.tf
+++ main.tf
@@ -68,13 +68,7 @@ module "alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = "aws.instance.blog.id"
-          port = 80
-        }
-       }
-    }
+      }
   ]
 
     http_tcp_listeners = [
