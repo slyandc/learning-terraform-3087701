97b92e84b792fd087d4d840939a650ca52682367
slyandc <18745271+slyandc@users.noreply.github.com>
2023-12-01 14:36:04 +0000

    use module for security group - sourced from registry.terraform.io v2

--- main.tf
+++ main.tf
@@ -34,10 +34,10 @@ module "blog_sg" {
   name         = "blog_new"
   vpc_id       = data.aws_vpc.default.id
 
-  ingress_rule          = ["http-80-tcp","http-443-tcp"]
+  ingress_rules          = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks   = ["0.0.0.0/0"]
 
-  egress_rule           = ["all-all"]
+  egress_rules           = ["all-all"]
   engress_cidr_blocks   = ["0.0.0.0/0"]
 
 }
