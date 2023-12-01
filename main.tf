119d32b579020722730e8a2430bd163d2e5b56c9
slyandc <18745271+slyandc@users.noreply.github.com>
2023-12-01 18:19:18 +0000

    remove alb target

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ module "autoscaling" {
 
   vpc_zone_identifier = module.blob_vpc.public_subnets
   target_group_arns   = module.blog_arns.target_group_arns
-  security_group      = [module.blog_sg.security_group_id]
+  security_groups      = [module.blog_sg.security_group_id]
 
   image_id            = data.aws_ami.app_ami.id
   instance_type       = var.instance_type
